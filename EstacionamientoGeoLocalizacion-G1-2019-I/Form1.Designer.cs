377f8519d558db62e3f6ee4866d9749ba77a6ae7
faraday58 <jarodriguezvera@gmail.com>
2018-10-05 16:48:38 -0500

    Agragando Listas

--- EstacionamientoGeoLocalizacion-G1-2019-I/Form1.Designer.cs
+++ EstacionamientoGeoLocalizacion-G1-2019-I/Form1.Designer.cs
@@ -28,13 +28,176 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtbLongitud = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbLatitud = new System.Windows.Forms.TextBox();
+            this.lstbDirecciones = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtbDireccion = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtbNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(37, 51);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(316, 320);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(398, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Longitud";
+            // 
+            // txtbLongitud
+            // 
+            this.txtbLongitud.Location = new System.Drawing.Point(499, 87);
+            this.txtbLongitud.Name = "txtbLongitud";
+            this.txtbLongitud.Size = new System.Drawing.Size(100, 20);
+            this.txtbLongitud.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Latitud";
+            // 
+            // txtbLatitud
+            // 
+            this.txtbLatitud.Location = new System.Drawing.Point(499, 132);
+            this.txtbLatitud.Name = "txtbLatitud";
+            this.txtbLatitud.Size = new System.Drawing.Size(100, 20);
+            this.txtbLatitud.TabIndex = 2;
+            // 
+            // lstbDirecciones
+            // 
+            this.lstbDirecciones.FormattingEnabled = true;
+            this.lstbDirecciones.Location = new System.Drawing.Point(401, 172);
+            this.lstbDirecciones.Name = "lstbDirecciones";
+            this.lstbDirecciones.Size = new System.Drawing.Size(198, 199);
+            this.lstbDirecciones.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(398, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Dirección";
+            // 
+            // txtbDireccion
+            // 
+            this.txtbDireccion.Location = new System.Drawing.Point(499, 43);
+            this.txtbDireccion.Name = "txtbDireccion";
+            this.txtbDireccion.Size = new System.Drawing.Size(100, 20);
+            this.txtbDireccion.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(622, 36);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(70, 32);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(718, 36);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(70, 32);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Nombre";
+            // 
+            // txtbNombre
+            // 
+            this.txtbNombre.Location = new System.Drawing.Point(499, 12);
+            this.txtbNombre.Name = "txtbNombre";
+            this.txtbNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtbNombre.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.lstbDirecciones);
+            this.Controls.Add(this.txtbLatitud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtbNombre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtbDireccion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbLongitud);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbLongitud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtbLatitud;
+        private System.Windows.Forms.ListBox lstbDirecciones;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtbDireccion;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtbNombre;
     }
 }
 
---
+++ Estacionamientos/Estacionamiento.cs
@@ -0,0 +1,27 @@
+﻿namespace Estacionamientos
+{
+    public class Estacionamiento
+    {
+        private string nombre;
+        private double longitud;
+        private double latitud;
+        private string direccion;
+
+       
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public double Longitud { get => longitud; set => longitud = value; }
+        public double Latitud { get => latitud; set => latitud = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+
+        public Estacionamiento(string nombre, double longitud, double latitud, string direccion)
+        {
+            Nombre = nombre;
+            Longitud = longitud;
+            Latitud = latitud;
+            Direccion = direccion;
+        }
+    }
+
+
+}
